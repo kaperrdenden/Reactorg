878fc6c02a95428b0438792009b7dfedce612e8b
kaperrdenden <weather2sogood@gmail.com>
2022-02-19 00:10:01 +0900

    React.createElement로 element생성

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,22 +4,14 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function formatName(user){
-  return user.firstName + ' ' + user.lastName;
-}
-const user = {
-  firstName: 'Harper',
-  lastName : 'Perez',
-}
 
-const element = <a href="https://www.reactjs.org"> link </a>;
-function getGreeting(user){
-  if (user){
-    return (<h1>Hello, {formatName(user)}</h1>)
-    
-  }
-  return <h1>Hello Starnger</h1>
-}
+
+const element = React.createElement(
+  'h1',
+  {className: 'greeting'},
+  'Hello World',
+)
+
 ReactDOM.render(
   element,
   document.getElementById('root')
