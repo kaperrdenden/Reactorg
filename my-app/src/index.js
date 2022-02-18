dbba60cee1c5aee76e0686eb9e61264ecbc7bba5
kaperrdenden <weather2sogood@gmail.com>
2022-02-18 23:59:03 +0900

    JSX의 어트리뷰트에 따옴표를 이용해 문자열 리터럴 정의

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -12,7 +12,7 @@ const user = {
   lastName : 'Perez',
 }
 
-const element = <h1>Hello {formatName(user)}</h1>
+const element = <a href="https://www.reactjs.org"> link </a>;
 function getGreeting(user){
   if (user){
     return (<h1>Hello, {formatName(user)}</h1>)
