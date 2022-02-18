635b8ef764d55d21d8b7fd1be0ed4146e2d72f22
kaperrdenden <weather2sogood@gmail.com>
2022-02-18 23:52:59 +0900

    변수 내부에 element를 넣고 그 element 내부에 표현식 사용하기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,8 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const name = "josh perez";
-const element = <h1>Hello {name}</h1>
+function formatName(user){
+  return user.firstName + ' ' + user.lastName;
+}
+const user = {
+  firstName: 'Harper',
+  lastName : 'Perez',
+}
+
+const element = <h1>Hello {formatName(user)}</h1>
 
 ReactDOM.render(
   element,
