c2d6393c4f69cb2ecda89da8ee4e066e4adb0ff0
kaperrdenden <weather2sogood@gmail.com>
2022-02-19 02:50:11 +0900

    Clock 제대로 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -6,17 +6,42 @@ import reportWebVitals from './reportWebVitals';
 import { Component } from 'react/cjs/react.production.min';
 
 
-function Welcome(props){
-  return(
-    <h1>Hello {props.name}</h1>
+class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      date:new Date()
+    };
+  }
+  componentDidMount(){
+    this.timerID = setInterval(()=>{ this.tick() }, 1000)
+  }
+  componentWillUnmount(){
+    clearInterval(this.timerID);
+  }
+  tick() {
+    this.setState({
+      date: new Date(),
+    })
+  }
+  render(){
+    return (
+      <div>
+        <h1>Hello, world</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    )
+  }
+  
+}
+function tick(){
+  ReactDOM.render(
+    <Clock />,
+    document.getElementById('root')
   )
 }
-const element = <Welcome name="sara"/>
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-  
+
+setInterval(tick,1000);
 
 
 
