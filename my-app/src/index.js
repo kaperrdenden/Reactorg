f1971bb0bb858610af79a1be45769d8b18dda02a
kaperrdenden <weather2sogood@gmail.com>
2022-02-18 23:50:37 +0900

    변수안에 element를 담기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,22 +4,13 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function tick(){
-  const element = (
-    <div>
-      <h1>Hello world</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+const name = "josh perez";
+const element = <h1>Hello {name}</h1>
 
-    </div>
-  )
-  ReactDOM.render(
-    element,
-    document.getElementById('root')
-  );
-}
-
-setInterval(tick, 1000);
-
+ReactDOM.render(
+  element,
+  document.getElementById('root')
+);
 
 
 // If you want to start measuring performance in your app, pass a function
