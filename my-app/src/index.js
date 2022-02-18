aa55bc6fcc7c34843ed3228657ef7fa67fae583b
kaperrdenden <weather2sogood@gmail.com>
2022-02-18 23:58:07 +0900

    유저의 유무에 따라서 다른 인사말 반환

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -13,7 +13,13 @@ const user = {
 }
 
 const element = <h1>Hello {formatName(user)}</h1>
-
+function getGreeting(user){
+  if (user){
+    return (<h1>Hello, {formatName(user)}</h1>)
+    
+  }
+  return <h1>Hello Starnger</h1>
+}
 ReactDOM.render(
   element,
   document.getElementById('root')
