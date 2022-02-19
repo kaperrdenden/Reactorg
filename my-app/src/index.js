9fb1205a3c81c5308f9f1343692bf01d0b23a365
kaperrdenden <weather2sogood@gmail.com>
2022-02-19 21:44:17 +0900

    스위치 버튼 만들기

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,45 +5,67 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Component } from 'react/cjs/react.production.min';
 
-
-class Clock extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      date:new Date()
-    };
-  }
-  componentDidMount(){
-    this.timerID = setInterval(()=>{ this.tick() }, 1000)
-  }
-  componentWillUnmount(){
-    clearInterval(this.timerID);
-  }
-  tick() {
-    this.setState({
-      date: new Date(),
-    })
-  }
-  render(){
-    return (
-      <div>
-        <h1>Hello, world</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    )
-  }
-  
+function UserGreeting(props) {
+  return <h1>Welcome back!</h1>;
 }
-function tick(){
-  ReactDOM.render(
-    <Clock />,
-    document.getElementById('root')
+
+function GuestGreeting(props) {
+  return <h1>Please sign up.</h1>;
+}
+function LoginButton(props) {
+  return (
+    <button onClick={props.onClick}>Login</button>
   )
 }
 
-setInterval(tick,1000);
+function LogoutButton(props) {
+  return (
+    <button onClick={props.onClick}>Logout</button>
+  )
+}
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
 
+class LoginControl extends React.Component {
+  constructor(props){
+    super(props);
+    this.handleLoginClick = this.handleLoginClick.bind(this);
+    this.handleLogoutClick = this.handleLogoutClick.bind(this);
+    this.state = {isLoggedIn: false};
+  }
+  handleLoginClick(){
+    this.setState({isLoggedIn: true})
+  }
+  handleLogoutClick() {
+    this.setState({isLoggedIn: false});
+  }
 
+  render() {
+    const isLoggedIn = this.state.isLoggedIn;
+    let button;
+    if(isLoggedIn){
+      button = <LogoutButton onClick={this.handleLogoutClick} />
+    } else {
+      button = <LoginButton  onClick={this.handleLoginClick} />
+    }
+    
+    return(
+      <div>
+        <Greeting isLoggedIn={isLoggedIn} />
+        {button}
+      </div>
+    )
+  }
+}
+ReactDOM.render(
+  <LoginControl />,
+  document.getElementById('root')
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
