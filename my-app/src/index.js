b8540211730da6206203e9ff0a4a78a26f5420c1
kaperrdenden <weather2sogood@gmail.com>
2022-02-19 22:24:38 +0900

    map을 사용해서 변수에 담고 해당 변수를 렌더링

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,65 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Component } from 'react/cjs/react.production.min';
 
-function UserGreeting(props) {
-  return <h1>Welcome back!</h1>;
-}
 
-function GuestGreeting(props) {
-  return <h1>Please sign up.</h1>;
-}
-function LoginButton(props) {
-  return (
-    <button onClick={props.onClick}>Login</button>
-  )
-}
+const number = [1,2,3,4,5];
+const listItems = number.map((number) => <li>{number}</li>);
 
-function LogoutButton(props) {
-  return (
-    <button onClick={props.onClick}>Logout</button>
-  )
-}
-function Greeting(props) {
-  const isLoggedIn = props.isLoggedIn;
-  if (isLoggedIn) {
-    return <UserGreeting />;
-  }
-  return <GuestGreeting />;
-}
 
-class LoginControl extends React.Component {
-  constructor(props){
-    super(props);
-    this.handleLoginClick = this.handleLoginClick.bind(this);
-    this.handleLogoutClick = this.handleLogoutClick.bind(this);
-    this.state = {isLoggedIn: false};
-  }
-  handleLoginClick(){
-    this.setState({isLoggedIn: true})
-  }
-  handleLogoutClick() {
-    this.setState({isLoggedIn: false});
-  }
-
-  render() {
-    const isLoggedIn = this.state.isLoggedIn;
-    let button;
-    if(isLoggedIn){
-      button = <LogoutButton onClick={this.handleLogoutClick} />
-    } else {
-      button = <LoginButton  onClick={this.handleLoginClick} />
-    }
-    
-    return(
-      <div>
-        <Greeting isLoggedIn={isLoggedIn} />
-        {button}
-      </div>
-    )
-  }
-}
 ReactDOM.render(
-  <LoginControl />,
+  <ul>{listItems}</ul>,
   document.getElementById('root')
 );
 
